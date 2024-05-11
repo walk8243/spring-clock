f728a35afbd21792571e1d9485f1c8949fc71346
walk8243 <walk8243@gmail.com>
2024-05-11 17:00:44 +0900

    spring initializr

---
+++ src/main/java/xyz/walk8243/springframework/clock/SpringClockApplication.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.springframework.clock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/walk8243/springframework/clock/SpringClockApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.springframework.clock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
