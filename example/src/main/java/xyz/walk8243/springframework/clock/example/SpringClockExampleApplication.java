e7b1ba37232022d733828292f2bf84cb00136ac2
walk8243 <walk8243@gmail.com>
2024-06-02 23:14:20 +0900

    exampleの追加

---
+++ example/src/main/java/xyz/walk8243/springframework/clock/example/SpringClockExampleApplication.java
@@ -0,0 +1,13 @@
+package xyz.walk8243.springframework.clock.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClockExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClockExampleApplication.class, args);
+	}
+
+}
