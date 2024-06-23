5e649aebd73eadc4db25f7f64e4d6c1c5940da0d
walk8243 <walk8243@gmail.com>
2024-06-23 16:33:34 +0900

    テストコードのタイムゾーンを調整

--- src/test/java/xyz/walk8243/springframework/clock/configuration/ClockConfigurationTest.java
+++ src/test/java/xyz/walk8243/springframework/clock/configuration/ClockConfigurationTest.java
@@ -1,10 +1,11 @@
 package xyz.walk8243.springframework.clock.configuration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.time.Clock;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Nested;
@@ -14,17 +15,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 class ClockConfigurationTest {
+	private static final DateTimeFormatter DATETIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ssZ");
 
 	@Nested
 	@SpringBootTest(classes = ClockConfiguration.class)
 	static class NoProperties {
-		@Autowired
-		private Clock clock;
+		@Autowired private ZoneId zoneId;
+		@Autowired private Clock clock;
 	
 		@Test
 		void contextLoads() {
+			assertEquals(ZoneId.systemDefault(), zoneId);
 			assertEquals(ZoneId.systemDefault(), clock.getZone());
-			assertEquals(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), LocalDateTime.now(clock).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+			assertEquals(ZonedDateTime.now().format(DATETIME_FORMAT), ZonedDateTime.now(clock).format(DATETIME_FORMAT));
 		}
 	}
 
@@ -34,14 +37,15 @@ class ClockConfigurationTest {
 		"spring.clock.timezone-id: Europe/London",
 	})
 	static class OnlyTimezone {
-		@Autowired
-		private Clock clock;
+		@Autowired private ZoneId zoneId;
+		@Autowired private Clock clock;
 	
 		@Test
 		void contextLoads() {
-			final ZoneId zoneId = ZoneId.of("Europe/London");
-			assertEquals(zoneId, clock.getZone());
-			assertEquals(LocalDateTime.now(zoneId).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), LocalDateTime.now(clock).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+			assertEquals(ZoneId.of("Europe/London"), zoneId);
+			assertEquals(ZoneId.of("Europe/London"), clock.getZone());
+			assertEquals(ZonedDateTime.now(zoneId).format(DATETIME_FORMAT), ZonedDateTime.now(clock).format(DATETIME_FORMAT));
+			assertNotEquals(ZonedDateTime.now().format(DATETIME_FORMAT), ZonedDateTime.now(clock).format(DATETIME_FORMAT));
 		}
 	}
 
@@ -51,30 +55,32 @@ class ClockConfigurationTest {
 		"spring.clock.mock-datetime: 2007-12-03T10:15:30",
 	})
 	static class OnlyMockDatetime {
-		@Autowired
-		private Clock clock;
+		@Autowired private ZoneId zoneId;
+		@Autowired private Clock clock;
 	
 		@Test
 		void contextLoads() {
+			assertEquals(ZoneId.systemDefault(), zoneId);
 			assertEquals(ZoneId.systemDefault(), clock.getZone());
-			assertEquals("2007-12-03T10:15:30", LocalDateTime.now(clock).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+			assertEquals("2007-12-03 10:15:30+0900", ZonedDateTime.now(clock).format(DATETIME_FORMAT));
 		}
 	}
 
 	@Nested
 	@SpringBootTest(classes = ClockConfiguration.class)
 	@TestPropertySource(properties = {
-		"spring.clock.timezone-id: Europe/London",
+		"spring.clock.timezone-id: America/Los_Angeles",
 		"spring.clock.mock-datetime: 2007-12-03T10:15:30",
 	})
 	static class TimezoneAndMockDatetime {
-		@Autowired
-		private Clock clock;
+		@Autowired private ZoneId zoneId;
+		@Autowired private Clock clock;
 	
 		@Test
 		void contextLoads() {
-			assertEquals(ZoneId.of("Europe/London"), clock.getZone());
-			assertEquals("2007-12-03T10:15:30", LocalDateTime.now(clock).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+			assertEquals(ZoneId.of("America/Los_Angeles"), zoneId);
+			assertEquals(ZoneId.of("America/Los_Angeles"), clock.getZone());
+			assertEquals("2007-12-03 10:15:30+-0800", ZonedDateTime.now(clock).format(DATETIME_FORMAT));
 		}
 	}
 }
